058e1ef9884aab6f2035c138389ecad4f1c6b816
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2020-06-26 17:48:16 +0530

    Jun 26  17:48:15

--- Assignment-1/Assignment3/testcases.py
+++ Assignment-1/Assignment3/testcases.py
@@ -1,3 +1,5 @@
 from file1 import *
 
 assert(add(2,3)) == 5
+
+assert add(1,2) == 3
--- Assignment-1/res.js
+++ Assignment-1/res.js
@@ -1,3 +1,4 @@
 var numberPattern = /\d+/g;
 let graderLine = '============================== 30 passed, 4 falied in 0.18s =============================='.match(numberPattern)
+
 console.log(graderLine);
