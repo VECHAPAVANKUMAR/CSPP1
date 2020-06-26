54334cac001101345a11598fd25fcf3579914e72
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2020-06-26 09:45:17 +0530

    Jun 26  09:45:17

---
+++ Assignment-1/res.js
@@ -0,0 +1,3 @@
+var numberPattern = /\d+/g;
+let graderLine = '============================== 30 passed, 4 falied in 0.18s =============================='.match(numberPattern)
+console.log(graderLine);
--- Assignment-1/testcases.py
+++
@@ -1,3 +0,0 @@
-from file1 import *
-
-assert(add(2,3)) == 5
