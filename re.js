f1be4af16329d21cea5781ff95932f7155b9e243
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2020-08-04 16:21:18 +0530

    Aug 04 16:21:14

---
+++ re.js
@@ -0,0 +1,404 @@
+let data = [
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:21:57 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"d",
+        position:"3,1",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:21:58 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"e",
+        position:"3,2",
+
+    }, 
+    
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:21:58 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"f",
+        position:"3,3",
+    }, 
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:21:59 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:" ",
+        position:"3,4",
+    }, 
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:00 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"a",
+        position:"3,5",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:00 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"r",
+        position:"3,6",
+    }, 
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:00 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"r",
+        position:"3,7",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:true,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:03 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"",
+        position:"3,7",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:true,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:03 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"",
+        position:"3,6",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:04 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"d",
+        position:"3,6",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:04 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"d",
+        position:"3,7",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:04 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"()",
+        position:"3,8",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:05 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"a",
+        position:"3,9",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:06 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:",",
+        position:"3,10",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:06 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"b",
+        position:"3,11",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:07 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:" ",
+        position:"3,13",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:08 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:":",
+        position:"3,14",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:10 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"\n\t",
+        position:"3,15",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:11 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"r",
+        position:"4,5",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:11 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"e",
+        position:"4,6",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:11 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"t",
+        position:"4,7",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:14 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"return",
+        position:"4,5",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:14 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:" ",
+        position:"4,11",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:15 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"a",
+        position:"4,12",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:15 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:" ",
+        position:"4,13",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:16 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"+",
+        position:"4,14",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:16 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:" ",
+        position:"4,15",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:17 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"b",
+        position:"4,16",
+    },
+
+    {
+        projectDir:"g:\Github\CSPP1\Assignment-1",
+        googleID:"101659395236687412917",
+        paste:false,
+        delete:false,
+        lines:0,
+        syntax:"python",
+        keyStrokeTimeStamp:"Fri Jul 31 2020 21:22:17 GMT+0530 (India Standard Time)",
+        keyStrokeInfo:"",
+        position:"4,17",
+
+    }
+
+];
+
+let linesInfo = {}
+let count = 0;
+data.forEach(obj => {
+    const row = obj.position.split(",")[0] - 1;
+    const col = obj.position.split(",")[1] - 1;
+    if (linesInfo[row] === undefined) {
+        linesInfo[row] = {}
+    }
+    if (linesInfo[row][col] !== undefined) {
+        for (let index = 1; index < obj.keyStrokeInfo.length; index++) {
+            if (linesInfo[row][col + index] !== obj.keyStrokeInfo.charAt(index)) {
+                break;
+            } else {
+                delete linesInfo[row][col + index]
+            }         
+        }
+    } 
+    linesInfo[row][col] = obj.keyStrokeInfo;
+    display(row, col);
+})
+function display(r, c) {
+    for (const row in linesInfo) {
+        if (linesInfo.hasOwnProperty(row)) {
+            const obj = linesInfo[row];
+            for (const col in obj) {
+                if (obj.hasOwnProperty(col)) {
+                    if (row == r && col == c) {
+                        console.log('Bold', linesInfo[r][c] + "|");
+                    } 
+                    else {
+                        console.log(linesInfo[row][col]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+// console.log(linesInfo)
+// for (let index = 0; index < Object.keys(linesInfo).length; index++) {
+//     const obj = linesInfo[String(index)];
+//     console.log(obj);
+//     // if (obj && obj.row === row && obj.col === col) {
+//     //     console.log("BOLD", obj[row][col]);
+//     // } else {
+
+//     // }
+// }
+
+
+// display();
